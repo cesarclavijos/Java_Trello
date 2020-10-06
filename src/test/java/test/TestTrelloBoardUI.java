9f2f3e349f6efdb995b02f456168d31a918a56aa
cesarclavijos <u1400568@unimilitar.edu.co>
2020-10-05 23:51:59 -0500

    change # 1

---
+++ src/test/java/test/TestTrelloBoardUI.java
@@ -0,0 +1,65 @@
+package test;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+import pages.*;
+
+
+public class TestTrelloBoardUI extends TestBaseUI {
+    private static LoguinPage loguinPage =new LoguinPage(driver) ;
+    private static ResultPage resultPage =new ResultPage(driver) ;
+    private static BoardPage boardPage =new BoardPage(driver) ;
+    private static CardPage cardPage =new CardPage(driver) ;
+
+
+    @BeforeClass
+    public static void  TrelloLoguin()  throws InterruptedException {
+        loguinPage.loguinTrello();
+        resultPage.SelectBoard();
+
+    }
+
+
+    @Test
+    public void AddCard() throws InterruptedException {
+        int TotalTicketInProgress = boardPage.CountTicketsInTodo();
+        boardPage.CreateaCard();
+        assert TotalTicketInProgress < boardPage.CountTicketsInTodo();
+
+    }
+
+    @Test
+    public void AddComent() throws InterruptedException {
+        boardPage.SelectFirstItemInToDo();
+        cardPage.AddDescription();
+        String TextoCard = cardPage.Description();
+    }
+
+
+    @Test
+    public void MovetoInProgress() throws InterruptedException {
+        boardPage.SelectFirstItemInToDo();
+        int TotalTicketInProgress = boardPage.CountTicketsInProgress();
+        cardPage.MovetoInProgress();
+        assert TotalTicketInProgress < boardPage.CountTicketsInProgress();
+    }
+
+    @Test
+    public void MovetoDone() throws InterruptedException {
+        boardPage.SelectFirstItemInProgress();
+        int TotalTicketInProgress = boardPage.CountTicketsInDone();
+        cardPage.MovetoInDone();
+        assert TotalTicketInProgress < boardPage.CountTicketsInDone();
+    }
+
+
+
+
+
+
+
+
+
+
+}
